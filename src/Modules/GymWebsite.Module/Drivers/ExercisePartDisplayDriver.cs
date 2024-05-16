4fb178e45417729a6d55a6913eccee72aa21b36f
BalazsVoros <balazs.t.voros@gmail.com>
2024-05-16 14:46:56 +0200

    added driver classes

---
+++ src/Modules/GymWebsite.Module/Drivers/ExercisePartDisplayDriver.cs
@@ -0,0 +1,50 @@
+﻿using GymWebsite.Module.Models;
+using GymWebsite.Module.ViewModels;
+using OrchardCore.ContentManagement.Display.ContentDisplay;
+using OrchardCore.ContentManagement.Display.Models;
+using OrchardCore.DisplayManagement.ModelBinding;
+using OrchardCore.DisplayManagement.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymWebsite.Module.Drivers
+{
+    public class ExercisePartDisplayDriver : ContentPartDisplayDriver<ExercisePart>
+    {
+        public override IDisplayResult Display(ExercisePart part, BuildPartDisplayContext context) =>
+           Initialize<ExercisePartViewModel>(
+               GetDisplayShapeType(context),
+               viewModel => PopulateViewModel(part, viewModel))
+           .Location("Detail", "Content:5")
+           .Location("Summary", "Content:5");
+
+        public override IDisplayResult Edit(ExercisePart part, BuildPartEditorContext context) =>
+            Initialize<ExercisePartViewModel>(
+                GetEditorShapeType(context),
+                viewModel => PopulateViewModel(part, viewModel))
+            .Location("Content:5");
+
+        public override async Task<IDisplayResult> UpdateAsync(ExercisePart part, IUpdateModel updater, UpdatePartEditorContext context)
+        {
+            var viewModel = new ExercisePartViewModel();
+
+            await updater.TryUpdateModelAsync(viewModel, Prefix);
+
+            part.Name = viewModel.Name;
+            part.Type = viewModel.Type;
+
+            return await EditAsync(part, context);
+        }
+
+
+        private static void PopulateViewModel(ExercisePart part, ExercisePartViewModel viewModel)
+        {
+            viewModel.Name = part.Name;
+            viewModel.Type = part.Type;
+
+        }
+    }
+}
---
+++ src/Modules/GymWebsite.Module/Drivers/MusclePagePartDisplayDriver.cs
@@ -0,0 +1,47 @@
+﻿using GymWebsite.Module.Models;
+using GymWebsite.Module.ViewModels;
+using OrchardCore.ContentManagement.Display.ContentDisplay;
+using OrchardCore.ContentManagement.Display.Models;
+using OrchardCore.DisplayManagement.ModelBinding;
+using OrchardCore.DisplayManagement.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymWebsite.Module.Drivers
+{
+    public class MusclePagePartDisplayDriver : ContentPartDisplayDriver<MusclePagePart>
+    {
+        public override IDisplayResult Display(MusclePagePart part, BuildPartDisplayContext context) =>
+           Initialize<MusclePagePartViewModel>(
+               GetDisplayShapeType(context),
+               viewModel => PopulateViewModel(part, viewModel))
+           .Location("Detail", "Content:5")
+           .Location("Summary", "Content:5");
+
+        public override IDisplayResult Edit(MusclePagePart part, BuildPartEditorContext context) =>
+            Initialize<MusclePagePartViewModel>(
+                GetEditorShapeType(context),
+                viewModel => PopulateViewModel(part, viewModel))
+            .Location("Content:5");
+
+        public override async Task<IDisplayResult> UpdateAsync(MusclePagePart part, IUpdateModel updater, UpdatePartEditorContext context)
+        {
+            var viewModel = new MusclePagePartViewModel();
+
+            await updater.TryUpdateModelAsync(viewModel, Prefix);
+
+            part.Name = viewModel.Name;
+
+            return await EditAsync(part, context);
+        }
+
+
+        private static void PopulateViewModel(MusclePagePart part, MusclePagePartViewModel viewModel)
+        {
+            viewModel.Name = part.Name;
+        }
+    }
+}
