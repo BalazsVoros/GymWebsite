c2d5c099831388185e3cd92c54df25ec0bd468c4
BalazsVoros <balazs.t.voros@gmail.com>
2024-05-16 14:47:38 +0200

    added viewmodel classes

---
+++ src/Modules/GymWebsite.Module/ViewModels/ExercisePartViewModel.cs
@@ -0,0 +1,19 @@
+﻿using GymWebsite.Module.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymWebsite.Module.ViewModels
+{
+    public class ExercisePartViewModel
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public ExerciseType Type { get; set; }
+    }
+}
---
+++ src/Modules/GymWebsite.Module/ViewModels/MusclePagePartViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymWebsite.Module.ViewModels
+{
+    public class MusclePagePartViewModel
+    {
+        [Required]
+        public string Name { get; set; }
+    }
+}
