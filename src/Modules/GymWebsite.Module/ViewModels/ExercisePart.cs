e3fd1a7928cee61ebc8595a8f0436232b3617054
BalazsVoros <balazs.t.voros@gmail.com>
2024-05-16 13:19:49 +0200

    added exercise model class

---
+++ src/Modules/GymWebsite.Module/ViewModels/ExercisePart.cs
@@ -0,0 +1,22 @@
+﻿using OrchardCore.ContentFields.Fields;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymWebsite.Module.ViewModels
+{
+    public class ExercisePart
+    {
+        public string Title { get; set; }
+        public TextField Description { get; set; }
+        public ExerciseType Type { get; set; }
+    }
+    public enum ExerciseType
+    {
+        FreeWeight,
+        Machine,
+    }
+
+}
