e7375f1dc1d8a9e79c08ef955334e00961251e24
BalazsVoros <balazs.t.voros@gmail.com>
2024-05-17 11:23:28 +0200

    added authorization

--- src/Modules/GymWebsite.Module/Controllers/AdminController.cs
+++ src/Modules/GymWebsite.Module/Controllers/AdminController.cs
@@ -1,6 +1,8 @@
 ﻿using GymWebsite.Module.Permissions;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using OrchardCore.ContentManagement;
+using OrchardCore.Contents;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,14 +14,26 @@ namespace GymWebsite.Module.Controllers
     public class AdminController : Controller
     {
         private readonly IAuthorizationService _authorizationService;
-        public AdminController(IAuthorizationService authorizationService)
+        private readonly IContentManager _contentManager;
+        public AdminController(IAuthorizationService authorizationService, IContentManager contentManager)
         {
 
             _authorizationService = authorizationService;
+            _contentManager = contentManager;
         }
         public string Index()
         {
             return "OK";
+        }
+
+        public async Task<string> ContentAuthorization()
+        {
+            var exercisepage = await _contentManager.NewAsync("ExercisePageFromCode");
+
+            var isAuthorized = await _authorizationService
+                .AuthorizeAsync(User, CommonPermissions.EditContent, exercisepage);
+
+            return $"Is authorized? {isAuthorized}";
         }
 
         public async Task<string> CustomAuthorization()
