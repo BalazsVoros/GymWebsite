0eb55b73d9554ab0d2b72b6c9cdade6cae62eb4a
BalazsVoros <balazs.t.voros@gmail.com>
2024-05-17 07:59:46 +0200

    added description for mucsle

--- src/Modules/GymWebsite.Module/Models/MusclePart.cs
+++ src/Modules/GymWebsite.Module/Models/MusclePart.cs
@@ -1,4 +1,5 @@
-﻿using OrchardCore.ContentManagement;
+﻿using OrchardCore.ContentFields.Fields;
+using OrchardCore.ContentManagement;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,5 +11,6 @@ namespace GymWebsite.Module.Models
     public class MusclePart : ContentPart
     {
         public string Name { get; set; }
+        public TextField Description { get; set; }
     }
 }
