18850cd8d05e1690cfb99b232c248b6721a9f22b
BalazsVoros <balazs.t.voros@gmail.com>
2024-05-17 06:34:26 +0200

    added handlers

---
+++ src/Modules/GymWebsite.Module/Handlers/ExercisePartHandler.cs
@@ -0,0 +1,19 @@
+﻿using GymWebsite.Module.Models;
+using OrchardCore.ContentManagement.Handlers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymWebsite.Module.Handlers
+{
+    public class ExercisePartHandler : ContentPartHandler<ExercisePart>
+    {
+        public override Task UpdatedAsync(UpdateContentContext context, ExercisePart instance)
+        {
+            context.ContentItem.DisplayText = instance.Name;
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ src/Modules/GymWebsite.Module/Handlers/MusclePartHandler.cs
@@ -0,0 +1,19 @@
+﻿using GymWebsite.Module.Models;
+using OrchardCore.ContentManagement.Handlers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymWebsite.Module.Handlers
+{
+    public class MusclePartHandler : ContentPartHandler<MusclePart>
+    {
+        public override Task UpdatedAsync(UpdateContentContext context, MusclePart instance)
+        {
+            context.ContentItem.DisplayText = instance.Name;
+            return Task.CompletedTask;
+        }
+    }
+}
